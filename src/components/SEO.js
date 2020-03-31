ab39a3f7d21563a73e2436e9c07c885a5fbceac5
Subin <subinsamrat96@gmail.com>
2020-03-31 21:29:25 +0530

    facebook cards meta added

--- src/components/SEO.js
+++ src/components/SEO.js
@@ -26,6 +26,14 @@ const SEO = ({title, description}) => {
     <Helmet htmlAttributes={{lang:'en'}} title={title}>
       <meta name="description" content={description}/>
       <meta name="image" content={image}/>
+      {/* facebook cards */}
+      <meta property="og:url" content={siteUrl}/>
+      <meta property="og:type" content="website"/>
+      <meta property="og:title" content={siteTitle}/>
+      <meta property="og:description" content={siteDesc}/>
+      <meta property="og:image" content={`${siteUrl}${image}`}/>
+      <meta property="og:image:width" content="400"/>
+      <meta property="og:image:height" content="300"/>
       {/* twitter cards */}
       <meta name="twitter:card" content="summary_large_image"/>
       <meta name="twitter:creator" content={twitterUsername}/>
