1cfd0ae04bf340519909b64bc6733033800e3b87
Subin <subinsamrat96@gmail.com>
2020-03-31 21:22:13 +0530

    twitter card meta added

--- src/components/SEO.js
+++ src/components/SEO.js
@@ -2,11 +2,36 @@ import React from 'react'
 import {Helmet} from 'react-helmet'
 import {useStaticQuery, graphql} from 'gatsby'
 
+const getData = graphql`
+query{
+  site{
+    siteMetadata{
+      siteTitle:title
+      siteDesc:description
+      author
+      twitterUsername
+      image
+      siteUrl
+    }
+  }
+}
+`
+
 
 const SEO = ({title, description}) => {
+  const {site} = useStaticQuery(getData);
+
+  const {siteTitle, siteDesc, siteUrl, image, twitterUsername} = site.siteMetadata;
   return (
     <Helmet htmlAttributes={{lang:'en'}} title={title}>
       <meta name="description" content={description}/>
+      <meta name="image" content={image}/>
+      {/* twitter cards */}
+      <meta name="twitter:card" content="summary_large_image"/>
+      <meta name="twitter:creator" content={twitterUsername}/>
+      <meta name="twitter:title" content={siteTitle}/>
+      <meta name="twitter:description" content={siteDesc}/>
+      <meta name="twitter:image" content={`${siteUrl}${image}`}/>
     </Helmet>
   )
 }
