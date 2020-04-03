11c7d807cd1ba72f6fe9f412a20fad9e8961be8b
Subin <subinsamrat96@gmail.com>
2020-04-03 22:21:00 +0530

    contact form changes v2.0

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -11,11 +11,23 @@ const Contact = () => {
         <h3 className={styles.contactme}>Contact Me</h3>
 
         <div className={styles.center}>
-          <form method="post" name="contact" data-netlify="true" data-netlify-honeypot="bot-field" className={styles.form}>
+          <form method="post" netlify-honeypot="bot-field" data-netlify="true" className={styles.form}>
+            <div>
+            <input type="hidden" name="bot-field" />
             <input type="text" name="name" id="name" className={styles.formControl} placeholder="Name..."/>
+          </div>
+
+          <div>
             <input type="email" name="email" id="email" className={styles.formControl} placeholder="Email..."/>
+          </div>
+
+          <div>
             <textarea name="message" rows="10" id="message" className={styles.formControl} placeholder="Message..."/>
+          </div>
+
+          <div>
             <input type="submit" name="submit" value="Submit here" className={styles.submit}/>
+          </div>
           </form>
         </div>
       </section>
