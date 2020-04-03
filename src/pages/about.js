95dbefe6027d8c27fef988ce102649577ea7ceea
Subin <subinsamrat96@gmail.com>
2020-04-03 22:01:47 +0530

    about and contact page added

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,12 +1,29 @@
 import React from 'react'
 import Layout from '../components/layout'
 import SEO from '../components/SEO'
+import styles from '../css/about.module.css'
+import img from '../images/myPhoto.jpg'
 
 const About = () => {
   return (
     <Layout>
       <SEO title="About | JS will rule" description="JavaScript will Rule About Page"/>
-      <h3>About page</h3>
+      <h3 className={styles.aboutme}>About Me</h3>
+      <div className={styles.aboutCenter}>
+        <article className={styles.aboutImg}>
+          <div className={styles.imgContainer}>
+            <img src={img} alt="my photo"/>
+          </div>
+        </article>
+
+        <article className={styles.aboutInfo}>
+          <h4>Front End Web Developer</h4>
+          <h5>Producing high-quality websites and exceptional user experience</h5>
+          <p>The work I provide is of highest quality, fully responsive, and tested in a wide range of devices. I take great care to ensure each project is well-documented and easily maintainable so you can enhance a website as your company grows.</p>
+
+          <p>Often front-end development of a website is overlooked, but being the part of your project that combines design, back-end development and the layer that your users interact with, from my experience, it is the most important to get right.</p>
+        </article>
+      </div>
     </Layout>
   )
 }
--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,12 +1,35 @@
 import React from 'react'
 import Layout from '../components/layout'
 import SEO from '../components/SEO'
+import styles from '../css/contact.module.css'
 
 const Contact = () => {
   return (
     <Layout>
       <SEO title="Contact | JS will rule" description="JavaScript will Rule Contact Page"/>
-      <h3>Contact page</h3>
+      <section className={styles.contact}>
+        <h3 className={styles.contactme}>Contact Me</h3>
+
+        <div className={styles.center}>
+          <form method="post" name="contact" data-netlify="true" data-netlify-honeypot="bot-field" className={styles.form}>
+            <div>
+            <input type="text" name="name" id="name" className={styles.formControl} placeholder="Name..."/>
+          </div>
+
+          <div>
+            <input type="email" name="email" id="email" className={styles.formControl} placeholder="Email..."/>
+          </div>
+
+          <div>
+            <textarea name="message" rows="10" id="message" className={styles.formControl} placeholder="Message..."/>
+          </div>
+
+          <div>
+            <input type="submit" value="Submit here" className={styles.submit}/>
+          </div>
+          </form>
+        </div>
+      </section>
     </Layout>
   )
 }
