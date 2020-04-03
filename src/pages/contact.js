c844d2c7ca797fd3d9e7d27762131492c1782c4a
Subin <subinsamrat96@gmail.com>
2020-04-03 22:10:22 +0530

    netlify form

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -12,21 +12,10 @@ const Contact = () => {
 
         <div className={styles.center}>
           <form method="post" name="contact" data-netlify="true" data-netlify-honeypot="bot-field" className={styles.form}>
-            <div>
             <input type="text" name="name" id="name" className={styles.formControl} placeholder="Name..."/>
-          </div>
-
-          <div>
             <input type="email" name="email" id="email" className={styles.formControl} placeholder="Email..."/>
-          </div>
-
-          <div>
             <textarea name="message" rows="10" id="message" className={styles.formControl} placeholder="Message..."/>
-          </div>
-
-          <div>
-            <input type="submit" value="Submit here" className={styles.submit}/>
-          </div>
+            <input type="submit" name="submit" value="Submit here" className={styles.submit}/>
           </form>
         </div>
       </section>
