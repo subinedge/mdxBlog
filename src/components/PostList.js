ba12eb596958875769d6e224476f75270f4e9da3
Subin <subinsamrat96@gmail.com>
2020-04-25 21:49:25 +0530

    newsletter design added

--- src/components/PostList.js
+++ src/components/PostList.js
@@ -6,9 +6,20 @@ const PostList = ({posts}) => {
   return (
     <section className={styles.posts}>
       <h1>Cookbook for FrontEnd Devs</h1>
-      {/* <h4>Always bet on JavaScript</h4> */}
-      {/* <h4>I have no special talent. I am only passionately curious</h4> */}
-      <h4>Once you stop learning you start dying</h4>
+      <h4>Always bet on JavaScript</h4>
+      {/* <h4>Once you stop learning you start dying</h4> */}
+
+      {/* newsletter */}
+
+      <div className={styles.newsletter}>
+        <h4>Get latest updates</h4>
+        {/* <h3>Join the list ! Understand JavaScript concepts</h3> */}
+
+        <form className={styles.form}>
+          <input type="email" required placeholder="Enter your email address"/>
+          <input type="submit" value="Subscribe"/>
+        </form>
+      </div>
 
       <div className={styles.center}>
         {posts.map(({node}, index)=> {
