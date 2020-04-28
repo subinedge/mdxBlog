0b0971d7209cd1c91db98df750ecd92cf61e495c
Subin <subinsamrat96@gmail.com>
2020-04-28 22:30:52 +0530

    google analytics plugin installed with config updated

--- gatsby-config.js
+++ gatsby-config.js
@@ -16,6 +16,12 @@ module.exports = {
       },
     },
     {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: process.env.GOOGLE_ANALYTICS_TRACKING_ID || 'none',
+      },
+    },
+    {
       resolve: 'gatsby-plugin-robots-txt',
       options: {
         host: 'https://javascriptwillrule.netlify.com',
