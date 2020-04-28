04f2c1c77316cbbc7bb85e5cc3991a2ab8df6b58
Subin <subinsamrat96@gmail.com>
2020-04-28 19:28:35 +0530

    aria label for error page link

--- src/pages/404.js
+++ src/pages/404.js
@@ -11,7 +11,7 @@ const Error = () => {
       <div className={styles.container}>
       <h1>404 - Page not found</h1>
         <h3>Oops ! You never know what content was in here, I removed it.</h3>
-        <Link to="/" className={styles.link}>Go back to home page</Link>
+        <Link to="/" aria-label="error page diversion link" className={styles.link}>Go back to home page</Link>
       </div>
     </Layout>
   )
