1d5284bdd6e38e555e61cfc144af12e47c5a684e
Subin <subinsamrat96@gmail.com>
2020-06-26 08:26:42 +0530

    blog card layout - thumbnail removed

--- src/components/PostCard.js
+++ src/components/PostCard.js
@@ -1,33 +1,38 @@
-import React from 'react'
-import styles from '../css/postcard.module.css'
-import Image from 'gatsby-image'
-import AniLink from "gatsby-plugin-transition-link/AniLink";
+import React from "react"
+import styles from "../css/postcard.module.css"
+import Image from "gatsby-image"
+import AniLink from "gatsby-plugin-transition-link/AniLink"
 
-const PostCard = ({post}) => {
-  const {title, slug, date, tag} = post.frontmatter;
-  const img = post.frontmatter.image.childImageSharp.fluid;
-  const excerpt = post.excerpt;
-  const timeToRead = post.timeToRead;
-  
+const PostCard = ({ post }) => {
+  const { title, slug, date, tag } = post.frontmatter
+  const img = post.frontmatter.image.childImageSharp.fluid
+  const excerpt = post.excerpt
+  const timeToRead = post.timeToRead
+
   return (
     <article className={styles.card}>
       <AniLink to={slug} aria-label="blog links" fade>
         <div className={styles.image}>
-          <Image fluid={img}/>
+          <Image fluid={img} />
         </div>
       </AniLink>
 
       <div className={styles.info}>
         <div>
-          <AniLink to={slug} fade aria-label="blog title" className={styles.headerLink}>
+          <AniLink
+            to={slug}
+            fade
+            aria-label="blog title"
+            className={styles.headerLink}
+          >
             <h2>{title}</h2>
           </AniLink>
           <h6>
             <span>{date}</span> | <span>{timeToRead} min read</span>
           </h6>
           <p className={styles.tag}>{tag}</p>
-          <p>{excerpt}</p>
-          
+          <p className={styles.excerpt}>{excerpt}</p>
+
           {/* <AniLink to={slug} fade aria-label="see full article" className={styles.link}>See full Article</AniLink> */}
         </div>
       </div>
