39166a7851ba813e2c726ccc17f9198447027e3c
Subin <subinsamrat96@gmail.com>
2020-07-23 22:47:12 +0530

    timeline on start of reactjs playlists added

--- src/constants/timeline.js
+++ src/constants/timeline.js
@@ -1,78 +1,99 @@
 export default [
   {
-    date:'June 07 2020',
-    tag:'YOUTUBE',
-    desc:'Posted a video on "How to become a Front End Developer in 2020 - A Roadmap"',
-    link:{
-      linkWords:'Check out the Video',
-      ariaLabel:'check out the roadmap video',
-      externalLink:'https://www.youtube.com/watch?v=6fcUD_HdvmQ',
+    date: "July 18 2020",
+    tag: "BLOG",
+    desc:
+      "Started REACT playlists and hoping to cover all major topics with real world projects",
+    link: {
+      linkWords: "Check out the React Playlists",
+      ariaLabel: "Check out the React Playlists",
+      externalLink:
+        "http://localhost:8000/reactjs-intro-tutorial-with-no-existing-react-knowledge",
     },
   },
 
   {
-    date:'June 06 2020',
-    tag:'VIRUS',
-    desc:'3 months since Coronavirus spread and still the total cases are increasing exponentially in India',
-    link:{
-      linkWords:'Coronavirus in India',
-      ariaLabel:'check out the total cases in India',
-      externalLink:'https://www.worldometers.info/coronavirus/country/india/',
+    date: "June 07 2020",
+    tag: "YOUTUBE",
+    desc:
+      'Posted a video on "How to become a Front End Developer in 2020 - A Roadmap"',
+    link: {
+      linkWords: "Check out the Video",
+      ariaLabel: "check out the roadmap video",
+      externalLink: "https://www.youtube.com/watch?v=6fcUD_HdvmQ",
     },
   },
 
   {
-    date:'May 05 2020',
-    tag:'BLOG',
-    desc:'Started my CSS playlists on decoding confused/tricky terms for web developers',
-    link:{
-      linkWords:'Check out my first post',
-      ariaLabel:'Check out first post',
-      externalLink:'https://www.javascriptwillrule.com/css-positioning-explained-by-building-project',
+    date: "June 06 2020",
+    tag: "VIRUS",
+    desc:
+      "3 months since Coronavirus spread and still the total cases are increasing exponentially in India",
+    link: {
+      linkWords: "Coronavirus in India",
+      ariaLabel: "check out the total cases in India",
+      externalLink: "https://www.worldometers.info/coronavirus/country/india/",
     },
   },
 
   {
-    date:'April 23 2020',
-    tag:'PRESENTATION',
-    desc:'I presented a session on Progressive web app to my colleagues through Microsoft Teams',
-    link:{
-      linkWords:'Check out the article',
-      ariaLabel:'pwa presentation blog',
-      externalLink:'https://javascriptwillrule.netlify.com/should-you-implement-pwa-in-your-website',
+    date: "May 05 2020",
+    tag: "BLOG",
+    desc:
+      "Started CSS playlists on decoding confused/tricky terms for web developers",
+    link: {
+      linkWords: "Check out my first post",
+      ariaLabel: "Check out first post",
+      externalLink:
+        "https://www.javascriptwillrule.com/css-positioning-explained-by-building-project",
     },
   },
 
   {
-    date:'April 10 2020',
-    tag:'MEDIUM/DEV',
-    desc:'Posted my first blog posts in Medium and dev.to',
-    link:{
-      linkWords:'Check out the blog',
-      ariaLabel:'Check out the blog',
-      externalLink:'https://medium.com/@javascriptwillrule/evaluate-reverse-polish-notation-expressions-using-javascript-algorithms-d8f8760fbaa1',
+    date: "April 23 2020",
+    tag: "PRESENTATION",
+    desc:
+      "I presented a session on Progressive web app to my colleagues through Microsoft Teams",
+    link: {
+      linkWords: "Check out the article",
+      ariaLabel: "pwa presentation blog",
+      externalLink:
+        "https://javascriptwillrule.netlify.com/should-you-implement-pwa-in-your-website",
     },
   },
 
   {
-    date:'March 29 2020',
-    tag:'BLOG',
-    desc:'Posted my first blog posts on Algorithms in JavaScript',
-    link:{
-      linkWords:'Check out the posts',
-      ariaLabel:'Check out the posts',
-      externalLink:'https://javascriptwillrule.netlify.com/reverse-polish-calculator-algorithm',
+    date: "April 10 2020",
+    tag: "MEDIUM/DEV",
+    desc: "Posted my first blog posts in Medium and dev.to",
+    link: {
+      linkWords: "Check out the blog",
+      ariaLabel: "Check out the blog",
+      externalLink:
+        "https://medium.com/@javascriptwillrule/evaluate-reverse-polish-notation-expressions-using-javascript-algorithms-d8f8760fbaa1",
     },
   },
 
   {
-    date:'March 20 2020',
-    tag:'BLOG',
-    desc:'Created my personal blog using Gatsby and MDX',
-    link:{
-      linkWords:'Check out my blog',
-      ariaLabel:'Check out my blog',
-      externalLink:'https://javascriptwillrule.netlify.com/',
+    date: "March 29 2020",
+    tag: "BLOG",
+    desc: "Posted my first blog posts on Algorithms in JavaScript",
+    link: {
+      linkWords: "Check out the posts",
+      ariaLabel: "Check out the posts",
+      externalLink:
+        "https://javascriptwillrule.netlify.com/reverse-polish-calculator-algorithm",
+    },
+  },
+
+  {
+    date: "March 20 2020",
+    tag: "BLOG",
+    desc: "Created my personal blog using Gatsby and MDX",
+    link: {
+      linkWords: "Check out my blog",
+      ariaLabel: "Check out my blog",
+      externalLink: "https://javascriptwillrule.netlify.com/",
     },
   },
 ]
