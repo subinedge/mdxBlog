6f261ce905742c181bb0ec85a7819d17cf0bdc6b
Subin <subinsamrat96@gmail.com>
2020-11-01 10:50:36 +0530

    class to functional components - subscribe form

--- src/components/Newsletter.js
+++ src/components/Newsletter.js
@@ -1,52 +1,46 @@
-import React from "react"
+import React, { useState } from "react"
 import styles from "../css/newsletter.module.css"
 
 import addToMailchimp from "gatsby-plugin-mailchimp"
 
-class Newsletter extends React.Component {
-  constructor(props) {
-    super(props)
-
-    this.state = {
-      email: "",
-      message: "Get my weekly posts to your mail",
-    }
-  }
+const Newsletter = () => {
+  const [input, setInput] = useState({
+    email: "",
+    message: "Get my weekly posts to your mail",
+  })
 
   changeEmailHandler = event => {
-    this.setState({ email: event.target.value })
+    setInput({ email: event.target.value })
   }
 
   handleSubmit = e => {
     e.preventDefault()
-    this.setState({ message: "Thanks for subscribing !" })
+    setInput({ message: "Thanks for subscribing !" })
 
-    addToMailchimp(this.state.email)
+    addToMailchimp(input.email)
 
-    this.setState({ email: "" }) // once submitted, clear the email input
+    setInput({ email: "" }) // once submitted, clear the email input
   }
 
-  render() {
-    return (
-      <>
-        <div className={styles.newsletter}>
-          {/* <h4 className={styles.messageHeader}>Get latest updates</h4> */}
-          <h3 className={styles.messageContent}>{this.state.message}</h3>
+  return (
+    <>
+      <div className={styles.newsletter}>
+        {/* <h4 className={styles.messageHeader}>Get latest updates</h4> */}
+        <h3 className={styles.messageContent}>{input.message}</h3>
 
-          <form className={styles.form} onSubmit={this.handleSubmit}>
-            <input
-              type="email"
-              value={this.state.email}
-              onChange={this.changeEmailHandler}
-              required
-              placeholder="Email address"
-            />
-            <input type="submit" value="Subscribe" />
-          </form>
-        </div>
-      </>
-    )
-  }
+        <form className={styles.form} onSubmit={handleSubmit}>
+          <input
+            type="email"
+            value={input.email}
+            onChange={changeEmailHandler}
+            required
+            placeholder="Email address"
+          />
+          <input type="submit" value="Subscribe" />
+        </form>
+      </div>
+    </>
+  )
 }
 
 export default Newsletter
